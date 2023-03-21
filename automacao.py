efaf1a6c661e2aa169f85024cd9340dc9487c040
eborgesfreitas <dudubfjj@gmail.com>
2023-03-21 08:13:31 -0300

    criando função

--- automacao.py
+++ automacao.py
@@ -14,61 +14,68 @@ nav = webdriver.Chrome(service=servico)
 tabela_produtos = pd.read_excel('buscas.xlsx')
 
 #pegar infos do produto
-
 produto = 'iphone 12 64 gb'
-produto = produto.lower()
 termos_banidos = 'mini watch'
-termos_banidos = termos_banidos.lower()
-lista_termos_nome_produto = produto.split(" ")
-lista_termos_banidos = termos_banidos.split(" ")
 preco_minimo = 3000
 preco_maximo = 4500
-lista_resultados = []
 
-#abrir navegador
-nav.get('https://www.google.com.br/')
-time.sleep(1)
-nav.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(produto, Keys.ENTER)
 
-#entrar na aba shopping
-elementos = nav.find_elements("class name", "hdtb-mitem")
-for item in elementos:
-    if 'Shopping' in item.text:
-        item.click()
-        break
+def busca_google_shopping(nav, produto, termos_banidos, preco_minimo, preco_maximo):
+    #tratando nomes banidos e palavras obrigatorias na busca
+    produto = produto.lower()
+    termos_banidos = termos_banidos.lower()
+    lista_termos_nome_produto = produto.split(" ")
+    lista_termos_banidos = termos_banidos.split(" ")
+    lista_ofertas = []
+    
+    #abrir navegador
+    nav.get('https://www.google.com.br/')
+    time.sleep(1)
+    nav.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(produto, Keys.ENTER)
 
-#pegar as informações do produto
-lista_resultados = nav.find_elements('class name', 'i0X6df')
+    #entrar na aba shopping
+    elementos = nav.find_elements("class name", "hdtb-mitem")
+    for item in elementos:
+        if 'Shopping' in item.text:
+            item.click()
+            break
 
-for resultado in lista_resultados:
-    nome = resultado.find_element('class name', 'tAxDx').text
-    nome = nome.lower()
+    #pegar as informações do produto
+    lista_resultados = nav.find_elements('class name', 'i0X6df')
 
-    #analisar termos banidos
-    tem_termos_banidos = False
-    for palavra in lista_termos_banidos:
-        if palavra in nome:
-            tem_termos_banidos = True
-            
-    #tem TODOS os termos do nome
-    tem_todos_termos_produtos = True
-    for palavra in lista_termos_nome_produto:
-        if palavra not in nome:
-            tem_todos_termos_produtos = False
-            
-    #selecionar apenas tem_termos_banidos = False e tem_todos_termos_produtos = True
-    if not tem_termos_banidos and tem_todos_termos_produtos:
-        #tratamento dos valores do preço
-        preco = resultado.find_element('class name', 'a8Pemb').text
-        preco = preco.replace("R$", "").replace(" ", "").replace(".", "").replace(",", ".")
-        preco = float(preco)
-        
-        preco_minimo = float(preco_minimo)
-        preco_maximo = float(preco_maximo)
-        #se o preco está entre minimo e maximo
-        if preco_minimo <= preco <= preco_maximo:
-            #pegando o link do produto
-            elemento_referencia = resultado.find_element('class name', 'bONr3b')
-            elemento_pai = elemento_referencia.find_element('xpath', '..')
-            link = elemento_pai.get_attribute('href')
-            print(preco, nome, link)
+    # nome do produto
+    for resultado in lista_resultados:
+        nome = resultado.find_element('class name', 'tAxDx').text
+        nome = nome.lower()
+
+        #analisar termos banidos
+        tem_termos_banidos = False
+        for palavra in lista_termos_banidos:
+            if palavra in nome:
+                tem_termos_banidos = True
+                
+        #analisar se tem TODOS os termos obrigatorios do nome
+        tem_todos_termos_produtos = True
+        for palavra in lista_termos_nome_produto:
+            if palavra not in nome:
+                tem_todos_termos_produtos = False
+                
+        #excluindo produtos com termos banidos e permitindo apenas os que possuam todos os nomes obrigatorios
+        if not tem_termos_banidos and tem_todos_termos_produtos:
+            #tratamento dos valores do preço
+            preco = resultado.find_element('class name', 'a8Pemb').text
+            preco = preco.replace("R$", "").replace(" ", "").replace(".", "").replace(",", ".")
+            preco = float(preco)
+            preco_minimo = float(preco_minimo)
+            preco_maximo = float(preco_maximo)
+            #se o preco está entre minimo e maximo
+            if preco_minimo <= preco <= preco_maximo:
+                #pegando o link do produto
+                elemento_referencia = resultado.find_element('class name', 'bONr3b')
+                elemento_pai = elemento_referencia.find_element('xpath', '..')
+                link = elemento_pai.get_attribute('href')
+                lista_ofertas.append((nome, preco, link))
+    return lista_ofertas
+
+lista_ofertas_google_shopping = busca_google_shopping(nav, produto, termos_banidos, preco_minimo, preco_maximo)
+print(lista_ofertas_google_shopping)
